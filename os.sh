1ce3aa5514a83dae56d1ac681d49fe04dc5670a9
cby <cby@chenby.cn>
2022-08-17 09:52:48 +0800

    Update os.sh

--- os.sh
+++ os.sh
@@ -14,7 +14,7 @@ function selinuxset(){
     grep SELINUX=disabled /etc/sysconfig/selinux
     getenforce
  fi
-    action "完成禁用SELINUX" /bin/true
+    echo "完成禁用SELINUX"
     echo "==========================================================="
     sleep 3
 }
@@ -49,7 +49,7 @@ function limitsset(){
     echo '* hard nofile 65536'
     echo '* soft nproc 65536'
     echo '* hard nproc 65536'
-    } >> file
+    } >> /etc/security/limits.conf
     echo '查看配置内容'
     cat /etc/security/limits.conf
     echo '设置软硬资源限制'
